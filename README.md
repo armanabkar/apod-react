03d132f7c9a373363956b76daaf4553a0be0c08c
Arman Abkar <armanabkar@gmail.com>
2022-01-15 14:21:40 +0330

    update styles

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Astronomy Picture of the Day
+<div align="center">
+	<img src="./public/favicon.png" width="200" height="200" />
+	<h1><strong>🚀🛰️ Astronomy Picture of the Day 🌌☄️</strong></h1>
+Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
+</div>
+<br>
+
+> Astronomy Picture of the Day is a website provided by NASA and Michigan Technological University. According to the website, "Each day a different image or photograph of our universe is featured, along with a brief explanation written by a professional astronomer." [Wikipedia](https://en.wikipedia.org/wiki/Astronomy_Picture_of_the_Day)
--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
               onChange={onChange}
               value={value}
               clearIcon={null}
+              minDate={new Date("1995-06-16")}
               maxDate={new Date()}
             />
             <h2 className="loading">
@@ -48,46 +49,58 @@ function App() {
           <h2 className="loading">Loading...</h2>
         )
       ) : (
-        <div className="main">
-          <DatePicker
-            onChange={onChange}
-            value={value}
-            clearIcon={null}
-            maxDate={new Date()}
-          />
-          <br />
-          {data.media_type === "video" ? (
-            <iframe src={data.url} title={data.title} />
-          ) : (
-            <a href={data.hdurl} target="_blank" rel="noopener noreferrer">
-              <img src={data.url} alt={data.title} />
-            </a>
-          )}
-          <h1>{data.title}</h1>
-          <h3>{data.copyright}</h3>
-          <p>
-            <span>Explanation: </span>
-            {data.explanation}
-          </p>
-          <hr />
-        </div>
+        <>
+          <div className="main">
+            <p className="intro">
+              Discover the cosmos! Each day a different image or photograph of
+              our fascinating universe is featured, along with a brief
+              explanation written by a professional astronomer.
+            </p>
+            <DatePicker
+              onChange={onChange}
+              value={value}
+              clearIcon={null}
+              minDate={new Date("1995-06-16")}
+              maxDate={new Date()}
+            />
+            <br />
+            {data.media_type === "video" ? (
+              <iframe src={data.url} title={data.title} />
+            ) : (
+              <a href={data.hdurl} target="_blank" rel="noopener noreferrer">
+                <img src={data.url} alt={data.title} />
+              </a>
+            )}
+            <h1>{data.title}</h1>
+            {data.copyright && (
+              <p>
+                <span>Image Credit: </span>
+                {data.copyright}
+              </p>
+            )}
+            <p>
+              <span>Explanation: </span>
+              {data.explanation}
+            </p>
+            <hr />
+          </div>
+          <footer>
+            <p>
+              This open-source project is powered by NASA Astronomy Picture of
+              the Day. You can get the source code from{" "}
+              <a
+                href="https://github.com/armanabkar/apod-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>
+              .
+            </p>
+            <p>Designed and Developed by Arman Abkar</p>
+          </footer>
+        </>
       )}
-
-      <footer>
-        <p>
-          This project is powered by NASA Astronomy Picture of the Day. You can
-          get the source code from{" "}
-          <a
-            href="https://github.com/armanabkar/apod-react"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            here
-          </a>
-          .
-        </p>
-        <p>Designed and Developed by Arman Abkar</p>
-      </footer>
     </>
   );
 }
